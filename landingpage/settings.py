3d08e30c2eb3ac4a2def5faa308c9ca418f7d4ad
kennya2709 <kennyorona27@gmail.com>
2023-11-15 16:01:23 -0600

    v2

--- landingpage/settings.py
+++ landingpage/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '*2izf_rnatia8yzrw&w_1()*0thnmwl1x%b#um)(04azi7*-x&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['3.142.50.180']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
